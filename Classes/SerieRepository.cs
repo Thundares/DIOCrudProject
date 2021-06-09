35a85f13a17ce1242d22c03ce4adaa4bbf888d45
Kenzo Mippo <pedrac.u@gmail.com>
2021-06-09 17:23:08 -0300

    Completed all the instructions of the second class

---
+++ Classes/SerieRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DIOCrud.Interface;
+
+namespace DIOCrud.Classes
+{
+    class SerieRepository : IRepository<Serie>
+    {
+        private List<Serie> myList = new List<Serie>();
+
+        public List<Serie> ReturnList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Serie obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int NextId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie ReturnById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Serie obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Interface/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIOCrud.Interface
+{
+    interface IRepository<T>
+    {
+        List<T> ReturnList();
+        T ReturnById(int id);
+        void Insert(T obj);
+        void Delete(int id);
+        void Update(int id, T obj);
+        int NextId();
+    }
+}
